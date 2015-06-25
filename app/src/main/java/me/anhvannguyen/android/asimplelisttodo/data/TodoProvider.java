239b19ed501fd2c6a1ea8d1784f1387a58da10ca
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-24 21:55:32 -0700

    create uri matcher

--- app/src/main/java/me/anhvannguyen/android/asimplelisttodo/data/TodoProvider.java
+++ app/src/main/java/me/anhvannguyen/android/asimplelisttodo/data/TodoProvider.java
@@ -2,15 +2,32 @@ package me.anhvannguyen.android.asimplelisttodo.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
 
 public class TodoProvider extends ContentProvider {
 
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private DBOpenHelper mOpenHelper;
+
+    private static final int TODO = 100;
+    private static final int TODO_WITH_ID = 101;
+
     @Override
     public boolean onCreate() {
+        mOpenHelper = new DBOpenHelper(getContext());
         return false;
+    }
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = TodoContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, TodoContract.PATH_TODO, TODO);
+        matcher.addURI(authority, TodoContract.PATH_TODO + "/#", TODO_WITH_ID);
+        return matcher;
     }
 
     @Override
