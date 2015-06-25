9999141655adecc762629bfff9c48129963a2e21
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-25 15:17:57 -0700

    insert test to the provider

--- app/src/main/java/me/anhvannguyen/android/asimplelisttodo/MainActivityFragment.java
+++ app/src/main/java/me/anhvannguyen/android/asimplelisttodo/MainActivityFragment.java
@@ -1,16 +1,23 @@
 package me.anhvannguyen.android.asimplelisttodo;
 
-import android.support.v4.app.Fragment;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import me.anhvannguyen.android.asimplelisttodo.data.TodoContract;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
 
     public MainActivityFragment() {
     }
@@ -18,6 +25,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return rootView;
+    }
+
+    // TODO: Move off of UI thread
+    public void insertTodo(String todoText) {
+        ContentValues newValue = new ContentValues();
+
+        newValue.put(TodoContract.TodoEntry.COLUMN_TEXT, todoText);
+        Uri insertUri = getActivity().getContentResolver().insert(
+                TodoContract.TodoEntry.CONTENT_URI,
+                newValue
+        );
+        Toast.makeText(getActivity(), "Inserting", Toast.LENGTH_LONG).show();
+        Log.d(LOG_TAG, "Inserted into " + insertUri.getLastPathSegment());
     }
 }
