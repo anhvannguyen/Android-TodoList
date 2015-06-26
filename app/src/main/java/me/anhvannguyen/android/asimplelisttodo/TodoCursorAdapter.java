3508e21766a3def0b2ebebe7d6a77fa250ac92b9
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-25 17:40:59 -0700

    create cursor adapter for the listview

--- app/src/main/java/me/anhvannguyen/android/asimplelisttodo/TodoCursorAdapter.java
+++ app/src/main/java/me/anhvannguyen/android/asimplelisttodo/TodoCursorAdapter.java
@@ -1,7 +1,50 @@
 package me.anhvannguyen.android.asimplelisttodo;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import me.anhvannguyen.android.asimplelisttodo.data.TodoContract;
+
 /**
  * Created by anhvannguyen on 6/25/15.
  */
-public class TodoCursorAdapter {
+public class TodoCursorAdapter extends CursorAdapter {
+
+    public TodoCursorAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_todo, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+
+        return view;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+        int todoIndex = cursor.getColumnIndex(TodoContract.TodoEntry.COLUMN_TEXT);
+        String todoItem = cursor.getString(todoIndex);
+        viewHolder.mTodoItemTextView.setText(todoItem);
+
+    }
+
+    // Viewholder to cache the children view
+    public static class ViewHolder {
+        TextView mTodoItemTextView;
+
+        public ViewHolder(View view) {
+            mTodoItemTextView = (TextView) view.findViewById(R.id.list_item_todo_textview);
+        }
+    }
 }
