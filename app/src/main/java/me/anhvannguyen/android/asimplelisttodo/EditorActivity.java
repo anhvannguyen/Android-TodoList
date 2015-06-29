913a13cd920133534fa780a9ecdb46036b0518a4
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-28 21:55:10 -0700

    replace action bar with toolbar

--- app/src/main/java/me/anhvannguyen/android/asimplelisttodo/EditorActivity.java
+++ app/src/main/java/me/anhvannguyen/android/asimplelisttodo/EditorActivity.java
@@ -1,17 +1,22 @@
 package me.anhvannguyen.android.asimplelisttodo;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class EditorActivity extends ActionBarActivity {
+public class EditorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
--- app/src/main/java/me/anhvannguyen/android/asimplelisttodo/MainActivity.java
+++ app/src/main/java/me/anhvannguyen/android/asimplelisttodo/MainActivity.java
@@ -1,17 +1,21 @@
 package me.anhvannguyen.android.asimplelisttodo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
     }
 
 
